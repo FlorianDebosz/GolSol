4372b6ebafa354dad457cf134f2a6eb16cc32c5a
Florian <Florian.debosz@outlook.fr>
2023-03-07 01:29:03 +0100

    ✨ Added Skybox rotation

--- Assets/Scripts/RandomSky.cs
+++ Assets/Scripts/RandomSky.cs
@@ -11,4 +11,8 @@ public class RandomSky : MonoBehaviour
         int random = Random.Range(0,skyMat.Length);
         RenderSettings.skybox = skyMat[random];
     }
+
+    private void Update() {
+        RenderSettings.skybox.SetFloat("_Rotation", 0.95f * Time.time);
+    }
 }
